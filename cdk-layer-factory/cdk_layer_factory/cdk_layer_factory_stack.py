bc5d5a8d23a2901addc454ee5e581b73cec6653d
James Shapiro <jamesshapiro@gmail.com>
2022-01-31 18:36:39 -0500

    add CDK for creating Layer Factory

---
+++ cdk-layer-factory/cdk_layer_factory/cdk_layer_factory_stack.py
@@ -0,0 +1,163 @@
+from aws_cdk import (
+    Duration,
+    aws_lambda as lambda_,
+    aws_iam as iam,
+    Stack,
+    aws_s3 as s3,
+    aws_stepfunctions as stepfunctions,
+    aws_stepfunctions_tasks as tasks,
+    aws_apigateway as apigateway
+)
+
+import aws_cdk as cdk
+
+from constructs import Construct
+
+class CdkLayerFactoryStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        with open('.cdk-params') as f:
+            lines = f.read().splitlines()
+            # .cdk-params should be of the form:
+            # account_id=12345678901234
+            account_id = [line for line in lines if line.startswith('account_id')][0].split('=')[1]
+
+        run_ec2_lambda_policy = iam.Policy(
+            self, 'cdk-layer-factory-start-creation-policy',
+            statements=[
+                # TODO: limit ec2 permissions
+                iam.PolicyStatement(
+                    actions=['ec2:*'],
+                    resources=['*']
+                ),
+                # TODO: limit PassRole to the ec2 role
+                iam.PolicyStatement(
+                    actions=['iam:PassRole'],
+                    resources=['*']
+                ),
+                
+            ]
+        )
+
+        layer_bucket = s3.Bucket(
+            self, "cdk-layer-factory-bucket",
+            encryption=s3.BucketEncryption.S3_MANAGED
+        )
+
+        ec2_role = iam.Role(self, "cdk-layer-factory-ec2-role",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            description="Allow EC2 instance to write to layer S3 bucket and publish layer versions to Lambda"
+        )
+
+        ec2_policy = iam.Policy(
+            self, 'cdk-layer-factory-ec2-s3-write-layer-publish-policy',
+            statements=[
+                iam.PolicyStatement(
+                    actions=['s3:PutObject'],
+                    resources=[layer_bucket.bucket_arn, f'{layer_bucket.bucket_arn}/*']
+                ),
+                iam.PolicyStatement(
+                    actions=['lambda:PublishLayerVersion'],
+                    resources=['*']
+                ),
+                # TODO: potentially narrow this with custom resources
+                iam.PolicyStatement(
+                    actions=['states:SendTaskSuccess','states:SendTaskFailure'],
+                    resources=['*']
+                ),
+            ]
+        )
+
+        ec2_role.attach_inline_policy(ec2_policy)
+        cfn_instance_profile = iam.CfnInstanceProfile(self, "MyCfnInstanceProfile",
+            roles=[ec2_role.role_name]
+        )
+
+        start_layer_creation_function_cdk = lambda_.Function(
+            self, 'cdk-layer-factory-start-creation',
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset('resources'),
+            handler='start_layer_creation.lambda_handler',
+            timeout=Duration.seconds(120),
+            memory_size=128,
+            environment={
+                'INSTANCE_PROFILE_ARN': cfn_instance_profile.attr_arn,
+                'LAYER_DEST_BUCKET': layer_bucket.bucket_name
+            }
+        )
+        start_layer_creation_function_cdk.role.attach_inline_policy(run_ec2_lambda_policy)
+
+        start_ec2_state = tasks.LambdaInvoke(self, "Start EC2",
+            lambda_function=start_layer_creation_function_cdk,
+            integration_pattern=stepfunctions.IntegrationPattern.WAIT_FOR_TASK_TOKEN,
+            payload=stepfunctions.TaskInput.from_object({
+                "token": stepfunctions.JsonPath.task_token,
+                "input": stepfunctions.JsonPath.string_at("$")
+            }),
+            result_path="$.taskresult",
+            timeout=Duration.hours(1)
+        )
+
+        # email_recipient = tasks.CallAwsService(self, "Send Email",
+        #     service="ses",
+        #     action="sendEmail",
+        #     parameters={
+        #         "Destination": {}
+        #         "Key": sfn.JsonPath.string_at("$.key")
+        #     },
+        #     iam_resources=[my_bucket.arn_for_objects("*")]
+        # )
+
+        definition = start_ec2_state
+
+        state_machine = stepfunctions.StateMachine(self, "cdk-sfn-demo-state-machine",
+            definition=definition
+        )
+
+        api = apigateway.RestApi(
+            self,
+            'cdk-lambda-layer-factory-api',
+            description='CDK Lambda Layer Factory.'
+        )
+
+        credentials_role = iam.Role(
+            self, 'cdk-sfn-demo-trigger-state-machine-role',
+            assumed_by=iam.ServicePrincipal("apigateway.amazonaws.com"),
+        )
+
+        trigger_state_machine_policy = iam.Policy(
+            self, 'cdk-sfn-demo-trigger-state-machine-policy',
+            statements=[iam.PolicyStatement(
+                actions=['states:StartExecution'],
+                resources=[state_machine.state_machine_arn]
+            )]
+        )
+        credentials_role.attach_inline_policy(trigger_state_machine_policy)
+
+        entry_point = api.root.add_resource("create-layer")
+        entry_point.add_method(
+            'POST',
+            integration=apigateway.AwsIntegration(
+                service='states',
+                action="StartExecution",
+                integration_http_method="POST",
+                options=apigateway.IntegrationOptions(
+                    credentials_role=credentials_role,
+                    request_templates={
+                        "application/json": f'{{"input": "$util.escapeJavaScript($input.body)", "stateMachineArn": "{state_machine.state_machine_arn}"}}'
+                    },
+                    integration_responses=[
+                        apigateway.IntegrationResponse(status_code="200")
+                    ],
+                )
+            ),
+            method_responses=[apigateway.MethodResponse(status_code="200")]
+        )
+
+        cdk.CfnOutput(
+            self, "StepFunctionsApi",
+            description="CDK Lambda Factory Entry Point API",
+            value = f'https://{api.rest_api_id}.execute-api.us-east-1.amazonaws.com/prod/create-layer/'
+        )
+
+
---
+++ cdk-layer-factory/resources/start_layer_creation.py
@@ -0,0 +1,83 @@
+import boto3
+import datetime
+import os
+
+ec2_client = boto3.client('ec2')
+iam_client = boto3.client('iam')
+ami_id = 'ami-0a8b4cd432b1c3063'
+instance_type = 't3.small'
+key_name = 'key.pem'
+subnet_id = 'subnet-c06751cf'
+
+instance_profile_arn = os.environ['INSTANCE_PROFILE_ARN']
+layer_dest_bucket = os.environ['LAYER_DEST_BUCKET']
+
+def lambda_handler(event, context):
+    token = event['token']
+    my_input = event['input']
+    now = datetime.datetime.now()
+    datetime_str = f'{now.year}-{now.month}-{now.day}-{now.hour}:{now.minute}:{now.second}'
+    # e.g. 'ulid-py'
+
+    lib_pip_name = my_input['lib_pip_name']
+    #colloquial_name = event['colloquial_name']
+    # e.g. '1.1.0'
+    version = my_input['version']
+    python_versions = my_input['python_versions']
+    init_script = [
+        '#!/bin/bash',
+        'sleep 60'
+        'cd ~'
+        'sudo yum update -y',
+        'sudo yum install ec2-instance-connect -y',
+        'sudo yum search docker',
+        'sudo yum install docker -y',
+        'sudo systemctl enable docker.service',
+        'sudo systemctl start docker.service',
+        'sudo systemctl status docker.service',
+        f'echo "{lib_pip_name}=={version}" >> requirements.txt',
+    ]
+    # e.g. ['python3.8', 'python3.9']
+    layer_publish_command = f'aws lambda publish-layer-version --layer-name {lib_pip_name}-{version.replace(".","-")}-layer-factory --description "{lib_pip_name}-{version} created by Layer Factory" --zip-file fileb://archive.zip --compatible-runtimes'
+    for python_version in python_versions:
+        init_script.append(f'mkdir -p "python/lib/{python_version}/site-packages/"')
+        init_script.append(f'docker run -v "$PWD":/var/task "public.ecr.aws/sam/build-{python_version}" /bin/sh -c "pip install -r requirements.txt -t python/lib/{python_version}/site-packages/; exit"')
+        layer_publish_command += f' "{python_version}"'
+    layer_publish_command += ' --region "us-east-1"'
+    init_script_wrapup = [
+        'zip -r archive.zip python > /dev/null',
+        f'aws s3 cp archive.zip s3://{layer_dest_bucket}/{lib_pip_name}-{version.replace(".","-")}-{datetime_str}',
+        layer_publish_command,
+        # TODO: make region configurable
+        f'aws stepfunctions send-task-success --task-token "{token}" --task-output \'{{"result": "Success!"}}\' --region us-east-1',
+        'shutdown -h now'
+    ]
+    init_script.extend(init_script_wrapup)
+    init_script = '\n ; \n'.join(init_script)
+    response = ec2_client.run_instances(
+        ImageId=ami_id,
+        InstanceType=instance_type,
+        IamInstanceProfile={
+            'Arn': instance_profile_arn
+        },
+        #KeyName=key_name,
+        SubnetId=subnet_id,
+        MaxCount=1,
+        MinCount=1,
+        InstanceInitiatedShutdownBehavior='terminate', 
+        UserData=init_script,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'APPLICATION',
+                        'Value': 'CDK_LAMBDA_LAYER_FACTORY'
+                    },
+                ]
+            },
+        ],
+    )
+    instance = response['Instances'][0]
+    instance_id = instance['InstanceId']
+    return {"result": "success creating EC2 instance", "instance_id": instance_id}
