324ff76c75fb537365bca9cd6bb4aff22ef24dd5
James Shapiro <jamesshapiro@gmail.com>
2022-02-26 16:22:51 -0500

    add simple input form hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,50 @@
-// aws s3 cp layerfactory-lambdalayerfactorycombucketef8645bd-dwllqbzsmy6p
-
+// npm run build && aws s3 cp --recursive build/ s3://layerfactory-lambdalayerfactorycombucketef8645bd-dwllqbzsmy6p && aws cloudfront create-invalidation --distribution-id E1LIL67TZCUW2P --paths "/*"
 import './App.css';
+import React from 'react'
+
+import { useInput } from './hooks/input-hook'
+
+export function NameForm(props) {
+  const {
+    value: firstName,
+    bind: bindFirstName,
+    reset: resetFirstName,
+  } = useInput('')
+  const {
+    value: lastName,
+    bind: bindLastName,
+    reset: resetLastName,
+  } = useInput('')
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    alert(`Submitting Name ${firstName} ${lastName}`)
+    resetFirstName()
+    resetLastName()
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" {...bindFirstName} />
+      </label>
+      <label>
+        Last Name:
+        <input type="text" {...bindLastName} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  )
+}
 
 function App() {
+  const url = process.env.REACT_APP_LAMBDA_LAYER_FACTORY_URL
   return (
     <div>
-      Shalom Haverim!
+      <div>Shalom Haverim! {url}</div>
+      <div><NameForm></NameForm></div>
     </div>
-  );
+  )
 }
 
 export default App;
